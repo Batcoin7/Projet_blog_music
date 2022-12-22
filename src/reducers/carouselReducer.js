4962e9025a8e8ddf6f133374366b152e628252d0
AESRajjaf <93983897+elyasrajjaf@users.noreply.github.com>
2022-12-22 09:53:39 +0100

    update lenght error

--- src/reducers/carouselReducer.js
+++ src/reducers/carouselReducer.js
@@ -1,20 +1,16 @@
-
-
 const initialState = {
-    length: 3
-}
+  length: 3,
+};
 
-const carouselReducer = ( state = initialState, action = {} ) => {
-    
-    switch( action.type )
-    {
-        case 'SET_INCREASE':
-            return action.payload > initialState.length ? 0 : action.payload;
-        case 'SET_DECREASE':
-            return action.payload < 0 ? initialState.length: action.payload;
-        default:
-            return state;
-    }
-}
+const carouselReducer = (state = initialState, action = {}) => {
+  switch (action.type) {
+    case "SET_INCREASE":
+      return action.payload > initialState.length ? 0 : action.payload;
+    case "SET_DECREASE":
+      return action.payload < 0 ? initialState.length : action.payload;
+    default:
+      return state;
+  }
+};
 
 export default carouselReducer;
