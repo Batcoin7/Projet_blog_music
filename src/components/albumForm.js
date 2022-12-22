158eb055e37f0b905a92eaae779d1f1b80fa71ef
AESRajjaf <93983897+elyasrajjaf@users.noreply.github.com>
2022-12-22 10:08:29 +0100

    update APP class -> className

--- src/components/albumForm.js
+++ src/components/albumForm.js
@@ -1,6 +1,8 @@
 import { useDispatch, useSelector } from "react-redux";
 import { addAlbum, setNewAlbum } from "../actions/albumActions";
 
+import "./stylesForm.css";
+
 const DragonForm = () => {
   const dispatch = useDispatch();
   const { newAlbum, errorMsgs } = useSelector((state) => state.albumReducer);
@@ -11,54 +13,55 @@ const DragonForm = () => {
   };
 
   return (
-    <form onSubmit={(e) => handleSubmit(e)}>
-      <h2>Ajouter Un Nouveau Album</h2>
-      {errorMsgs.length > 0 && (
-        <ul className="error-box">
-          {errorMsgs.map((error, i) => (
-            <li key={i}>{error}</li>
-          ))}
-        </ul>
-      )}
+    <div className="containerForm">
+      <form onSubmit={(e) => handleSubmit(e)} className="Formulaire">
+        <h2>Ajouter Un Nouveau Album</h2>
+        {errorMsgs.length > 0 && (
+          <ul className="error-box">
+            {errorMsgs.map((error, i) => (
+              <li key={i}>{error}</li>
+            ))}
+          </ul>
+        )}
 
-      <div className="labelForm">
-        <label htmlFor="title">Titre d'album:</label>
-        <input
-          name="title"
-          type="text"
-          value={newAlbum.title}
-          onChange={(e) =>
-            dispatch(
-              setNewAlbum({
-                newAlbum: {
-                  ...newAlbum,
-                  title: e.target.value,
-                },
-              })
-            )
-          }
-        />
-      </div>
-      <div className="labelForm">
-        <label htmlFor="image">Insérer l'image d'album:</label>
-        <input
-          name="image"
-          type="image"
-          alt={newAlbum.title}
-          value={newAlbum.image}
-          onChange={(e) =>
-            dispatch(
-              setNewAlbum({
-                newAlbum: {
-                  ...newAlbum,
-                  image: e.target.value,
-                },
-              })
-            )
-          }
-        />
-      </div>
-      {/* <p>
+        <div className="labelForm">
+          <label htmlFor="title">Titre d'album:</label>
+          <input
+            name="title"
+            type="text"
+            value={newAlbum.title}
+            onChange={(e) =>
+              dispatch(
+                setNewAlbum({
+                  newAlbum: {
+                    ...newAlbum,
+                    title: e.target.value,
+                  },
+                })
+              )
+            }
+          />
+        </div>
+        <div className="labelForm">
+          <label htmlFor="image">Insérer l'image d'album:</label>
+          <input
+            name="image"
+            type="image"
+            alt={newAlbum.title}
+            value={newAlbum.image}
+            onChange={(e) =>
+              dispatch(
+                setNewAlbum({
+                  newAlbum: {
+                    ...newAlbum,
+                    image: e.target.value,
+                  },
+                })
+              )
+            }
+          />
+        </div>
+        {/* <p>
                 <label htmlFor="categorie">Categorie</label>
                 <select name="categorie"
                         value={newAlbum.categorie}
@@ -70,28 +73,29 @@ const DragonForm = () => {
                     { categorie.map( (category, i) => <option key={i} value={category}>{category}</option> ) }
                 </select>
             </p> */}
-      <div className="labelForm">
-        <label htmlFor="artiste">Nom de l'artiste:</label>
-        <input
-          name="artiste"
-          type="text"
-          value={newAlbum.artiste}
-          onChange={(e) =>
-            dispatch(
-              setNewAlbum({
-                newAlbum: {
-                  ...newAlbum,
-                  artiste: e.target.value,
-                },
-              })
-            )
-          }
-        />
-      </div>
-      <div className="labelForm">
-        <button type="submit">Ajouter l'ablbum</button>
-      </div>
-    </form>
+        <div className="labelForm">
+          <label htmlFor="artiste">Nom de l'artiste:</label>
+          <input
+            name="artiste"
+            type="text"
+            value={newAlbum.artiste}
+            onChange={(e) =>
+              dispatch(
+                setNewAlbum({
+                  newAlbum: {
+                    ...newAlbum,
+                    artiste: e.target.value,
+                  },
+                })
+              )
+            }
+          />
+        </div>
+        <div className="labelForm">
+          <button type="submit">Ajouter l'ablbum</button>
+        </div>
+      </form>
+    </div>
   );
 };
 
