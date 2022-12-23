6dc8a26df507a223f69d694ca96912df338441fe
AESRajjaf <93983897+elyasrajjaf@users.noreply.github.com>
2022-12-23 11:47:48 +0100

    Update albums.js

--- src/components/albums.js
+++ src/components/albums.js
@@ -12,39 +12,40 @@ const Albums = () => {
   return (
     <>
       <MyCarousel />
-      <div className="container-wrap">
-      <ul>
-        {albums.map((album, i) => (
-          <li key={i}>
-            
-            <div
-              onMouseEnter={() => dispatch(toggleDetails({ album }))}
-              onMouseLeave={() => dispatch(toggleDetails({ album }))}
-            >
-              <img
-                src={album.image}
-                style={!album.show ? { display: "block" } : { display: "none" }}
-              />
+      <div className="container-wrap"  style={{ display: "flex", justifyContent:"center"}}>
+        <h1 style={{ textAlign: "center" }}>Liste d'albums</h1>
+        <ul style={{ display: "flex", flexWrap: "wrap" }}>
+          {albums.map((album, i) => (
+            <li key={i}>
+              
               <div
-                style={album.show ? { display: "block" } : { display: "none" }}
+                onMouseEnter={() => dispatch(toggleDetails({ album }))}
+                onMouseLeave={() => dispatch(toggleDetails({ album }))}
               >
-                <h4>{album.title}</h4>
-                <h5>{album.artiste}</h5>
-                <h6>{album.date}</h6>
-                <h6>{album.categorie}</h6>
-                <strong>
-                  {album.son.map((s, i) => (
-                    <h6 key={i}>{s}</h6>
-                  ))}
-                </strong>
-                <button type="button" class="btn btn-outline-primary" onClick={() => dispatch(deleteAlbum(album))}>
-              Supprimer
-            </button>
+                <img
+                  src={album.image}
+                  style={!album.show ? { display: "block" } : { display: "none" }}
+                />
+                <div
+                  style={album.show ? { display: "block" } : { display: "none" }}
+                >
+                  <h4>{album.title}</h4>
+                  <h5>{album.artiste}</h5>
+                  <h6>{album.date}</h6>
+                  <h6>{album.categorie}</h6>
+                  <strong>
+                    {album.son.map((s, i) => (
+                      <h6 key={i}>{s}</h6>
+                    ))}
+                  </strong>
+                  <button type="button" class="btn btn-outline-primary" onClick={() => dispatch(deleteAlbum(album))}>
+                Supprimer
+              </button>
+                </div>
               </div>
-            </div>
-          </li>
-        ))}
-      </ul>
+            </li>
+          ))}
+        </ul>
       </div>
     </>
   );
